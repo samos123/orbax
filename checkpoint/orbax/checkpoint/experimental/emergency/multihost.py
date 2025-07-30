cbd8e24716705293f7eab26a533f3c6891e304f9
Sam Stoelinga <sammiestoel@gmail.com>
2025-07-30 09:48:48 -0700

    cherry-pick of Jun's patch on 0.11.20
    https://github.com/findmyway/orbax/commit/df6c7691fb689273bb4905ed033947d4ee239d27#diff-7644e8c1b3602bcc0a5e2e73cb66012caee01104d1abcf6174ecc6bb7258c7a1

--- checkpoint/orbax/checkpoint/experimental/emergency/multihost.py
+++ checkpoint/orbax/checkpoint/experimental/emergency/multihost.py
@@ -132,26 +132,32 @@ def consistent_restore_mesh(
   Returns:
     The new mesh devices that should be used to create the mesh.
   """
-  # Map how device ids changed across restarts.
-  device_id_across_restarts = {}
-  for i in range(len(previous_distributed_to_device_ids)):
-    for j in range(len(previous_distributed_to_device_ids[i])):
-      previous_id = previous_distributed_to_device_ids[i][j]
-      current_id = current_distributed_to_device_ids[i][j]
-      device_id_across_restarts[previous_id] = current_id
-  logging.debug(
-      'device_id_across_restarts (key: previous_id, value: current_id): %s',
-      device_id_across_restarts,
-  )
-  # Key devices by id.
-  jax_devices_by_id = {d.id: d for d in devices}
+  # # Map how device ids changed across restarts.
+  # device_id_across_restarts = {}
+  # for i in range(len(previous_distributed_to_device_ids)):
+  #   for j in range(len(previous_distributed_to_device_ids[i])):
+  #     previous_id = previous_distributed_to_device_ids[i][j]
+  #     current_id = current_distributed_to_device_ids[i][j]
+  #     device_id_across_restarts[previous_id] = current_id
+  # logging.debug(
+  #     'device_id_across_restarts (key: previous_id, value: current_id): %s',
+  #     device_id_across_restarts,
+  # )
+  # # Key devices by id.
+  # jax_devices_by_id = {d.id: d for d in devices}
 
-  new_flattened_mesh_devices = [
-      # Convert old ids to current ids that correspond to the same physical
-      # hardware.
-      jax_devices_by_id[device_id_across_restarts[id]]
-      for id in previous_flattened_mesh_device_ids
-  ]
+  # new_flattened_mesh_devices = [
+  #     # Convert old ids to current ids that correspond to the same physical
+  #     # hardware.
+  #     jax_devices_by_id[device_id_across_restarts[id]]
+  #     for id in previous_flattened_mesh_device_ids
+  # ]
+  # new_mesh_devices = np.array(new_flattened_mesh_devices).reshape(
+  #     user_mesh.devices.shape
+  # )
+  # return jax.sharding.Mesh(new_mesh_devices, user_mesh.axis_names)
+  device_ids = [x.id for x in devices]
+  new_flattened_mesh_devices = [devices[device_ids.index(i)] for i in previous_flattened_mesh_device_ids]
   new_mesh_devices = np.array(new_flattened_mesh_devices).reshape(
       user_mesh.devices.shape
   )
